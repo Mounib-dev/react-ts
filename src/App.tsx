20c1059ba24e4352fa3877d6f47d99ef702e204c
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-01-22 21:04:37 +0100

    Adding NewTodo component with submit handling function and a ref to extract user input by adding the correct types

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+
 const todos = [{ id: "t1", text: "Learn React and TypeScript" }];
 // FC Type is a Function Component
 const App: React.FC = () => {
   return (
     <>
       <div className="App">
+        <NewTodo />
         <TodoList items={todos} />
       </div>
     </>
