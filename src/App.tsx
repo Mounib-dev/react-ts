15f6574ef5ada84e0ff748f6fcb978357e0fcb3d
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-01-22 20:56:14 +0100

    Adding a trivial todolist and type it

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import TodoList from "./components/TodoList";
+const todos = [{ id: "t1", text: "Learn React and TypeScript" }];
+// FC Type is a Function Component
 const App: React.FC = () => {
   return (
     <>
-      <div className="App"></div>
+      <div className="App">
+        <TodoList items={todos} />
+      </div>
     </>
   );
 };
