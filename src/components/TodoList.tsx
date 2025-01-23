0f64be1555ebbe04e9ea64caa063ee397a01d0e1
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-01-23 08:23:35 +0100

    Handling delete of todo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 interface TodoListProps {
   items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
 }
 
 // FC is a Generic Type so we can create an Interface or a Type and "feed" it to the FC Gen. Type
@@ -10,7 +11,12 @@ const TodoList: React.FC<TodoListProps> = (props) => {
     <>
       <ul>
         {props.items.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
+          <li key={todo.id}>
+            <span>{todo.text}</span>
+            <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+              DELETE
+            </button>
+          </li>
         ))}
       </ul>
     </>
